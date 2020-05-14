b1e4f3819ee91af97eed1c8af88e0e15304f9d29
Andrew Seedholm <aseedholm@gmail.com>
2020-05-14 17:42:25 -0400

    All searches now work with provided inputs. Only one search is currently linked to the search button, searching by zip code. Search results are displayed under the search bar as well.

--- src/search/CitySearch.js
+++ src/search/CitySearch.js
@@ -15,15 +15,19 @@ export default class CitySearch extends React.Component {
 
     state = {
         //Search field works by itself, cannot guarantee correct city.
-        cityName: ' ',
+        cityName: '',
         //Search field only works in conjunction with city.
         stateName: ' ',
         //Search field works by itself
-        zipCode: ' ',
+        zipCode: '',
         //API key required by API for calls.
         apiKey: 'fb71a69f93b29d7c3ad5ca6d0270914b',
         //Information for returned object.
-        cityWeather: {}
+        cityWeather: {},
+        //Used to identify if something was searched.
+        searched: false,
+        //Used to determine if user wants temperature in celsius (metric) or fahrenheit (imperial).
+        temperatureUnit: 'imperial'
     }
 
 
@@ -44,7 +48,7 @@ export default class CitySearch extends React.Component {
      * @return {String}         The weather data as a String.
      */
     findCityByName = (cityName) => {
-        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${this.state.apiKey}`)
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${this.state.apiKey}&units=${this.state.temperatureUnit}`)
             .then(response => response.json())
             // .then(results => this.setState({
             //                                    //TODO: change state using prevState.
@@ -65,7 +69,7 @@ export default class CitySearch extends React.Component {
      */
     findCityByNameAndState = (cityName, stateName) => {
         //Need to specify country ID otherwise it may not find a result in the United States.
-        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateName},US&appid=${this.state.apiKey}`)
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateName},US&appid=${this.state.apiKey}&units=${this.state.temperatureUnit}`)
             .then(response => response.json())
             // .then(results => this.setState({
             //                                    //TODO: change state using prevState.
@@ -85,35 +89,14 @@ export default class CitySearch extends React.Component {
     findCityByZipCode = (zipCode) => {
         //API defaults to US as country code, don't need to specify it.
         //Specifying was also causing issues with the fetch request.
-        fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${this.state.apiKey}`)
+        fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${this.state.apiKey}&units=${this.state.temperatureUnit}`)
             .then(response => response.json())
-            // .then(results => this.setState({
-            //                                    //TODO: change state using prevState.
-            //                                    cityWeather: results
-            //                                }))
-            .then(resultsTwo => console.log(resultsTwo))
+            // .then(results => console.log(results))
+            .then(results => this.setState({
+                                               //TODO: change state using prevState.
+                                               cityWeather: results
+                                           }))
     }
-
-    /**
-     * This function calls the fetch function contained in the weatherServices class based on the
-     * city name, state name, and zip code searched for by a user. It returns a JSON object in
-     * String form, containing weather data for the searched city.
-     * @param {String} cityName the name of the city being searched for.
-     * @param {String} stateName the name of the State the city resides in.
-     * @param {String} zipCode the zip of the city being searched for.
-     *
-     * @return {String}         The weather data as a String.
-     */
-    findCityByNameAndStateAndZipCode = (cityName, stateName, zipCode) => {
-        fetch(`api.openweathermap.org/data/2.5/weather?q=${cityName},${stateName},US&appid={your api key}&appid=${this.state.apiKey}`)
-            .then(response => response.json())
-            // .then(results => this.setState({
-            //                                    //TODO: change state using prevState.
-            //                                    cityWeather: results
-            //                                }))
-            .then(resultsTwo => console.log(resultsTwo))
-    }
-
 
     render() {
         return (
@@ -121,34 +104,145 @@ export default class CitySearch extends React.Component {
                 <h1>
                     SEARCH PAGE
                 </h1>
+                <select className="state-select"
+                        onChange={(e) => {
+                            this.setState({
+                                              temperatureUnit: e.target.value
+                                          })
+                        }}
+                        value={this.state.temperatureUnit}>
+                    <option value="metric">Celsius</option>
+                    <option value="imperial">Fahrenheit</option>
+                </select>
+                <br/>
+
+                <input type="text"
+                       placeholder="City Name"
+                       className="city-name-input"
+                    //TODO change setState to use prevState.
+                       onChange={(e) => this.setState({
+                                                          cityName: e.target.value
+                                                      })}
+                       value={this.state.cityName}/>
+                {/*<button onClick={ () => {*/}
+                {/*    this.findCityByName(this.state.cityName)*/}
+                {/*    console.log(this.state.cityName)*/}
+                {/*}}>*/}
+                {/*    TESTING BUTTON CITY NAME*/}
+                {/*</button>*/}
+                {/*<br/>*/}
+
+                {/*Found at https://www.freeformatter.com/usa-state-list-html-select.html*/}
+                <select className="state-select"
+                        onChange={(e) => {
+                            this.setState({
+                                stateName: e.target.value
+                                          })
+                        }}
+                        value={this.state.stateName}>
+                    <option value="AL">Alabama</option>
+                    <option value="AK">Alaska</option>
+                    <option value="AZ">Arizona</option>
+                    <option value="AR">Arkansas</option>
+                    <option value="CA">California</option>
+                    <option value="CO">Colorado</option>
+                    <option value="CT">Connecticut</option>
+                    <option value="DE">Delaware</option>
+                    <option value="DC">District Of Columbia</option>
+                    <option value="FL">Florida</option>
+                    <option value="GA">Georgia</option>
+                    <option value="HI">Hawaii</option>
+                    <option value="ID">Idaho</option>
+                    <option value="IL">Illinois</option>
+                    <option value="IN">Indiana</option>
+                    <option value="IA">Iowa</option>
+                    <option value="KS">Kansas</option>
+                    <option value="KY">Kentucky</option>
+                    <option value="LA">Louisiana</option>
+                    <option value="ME">Maine</option>
+                    <option value="MD">Maryland</option>
+                    <option value="MA">Massachusetts</option>
+                    <option value="MI">Michigan</option>
+                    <option value="MN">Minnesota</option>
+                    <option value="MS">Mississippi</option>
+                    <option value="MO">Missouri</option>
+                    <option value="MT">Montana</option>
+                    <option value="NE">Nebraska</option>
+                    <option value="NV">Nevada</option>
+                    <option value="NH">New Hampshire</option>
+                    <option value="NJ">New Jersey</option>
+                    <option value="NM">New Mexico</option>
+                    <option value="NY">New York</option>
+                    <option value="NC">North Carolina</option>
+                    <option value="ND">North Dakota</option>
+                    <option value="OH">Ohio</option>
+                    <option value="OK">Oklahoma</option>
+                    <option value="OR">Oregon</option>
+                    <option value="PA">Pennsylvania</option>
+                    <option value="RI">Rhode Island</option>
+                    <option value="SC">South Carolina</option>
+                    <option value="SD">South Dakota</option>
+                    <option value="TN">Tennessee</option>
+                    <option value="TX">Texas</option>
+                    <option value="UT">Utah</option>
+                    <option value="VT">Vermont</option>
+                    <option value="VA">Virginia</option>
+                    <option value="WA">Washington</option>
+                    <option value="WV">West Virginia</option>
+                    <option value="WI">Wisconsin</option>
+                    <option value="WY">Wyoming</option>
+                </select>
+                {/*<button onClick={ () => {*/}
+                {/*    this.findCityByNameAndState(this.state.cityName, this.state.stateName)*/}
+                {/*    console.log("CITY", this.state.cityName, "---------- STATE", this.state.stateName)*/}
+                {/*}}>*/}
+                {/*    TESTING BUTTON CITY + STATE*/}
+                {/*</button>*/}
+                {/*<br/>*/}
+                <input type="text"
+                       placeholder="Zip Code"
+                       className="zip-code-input"
+                    //TODO change setState to use prevState.
+                       onChange={(e) => this.setState({
+                                                          zipCode: e.target.value
+                                                      })}
+                       value={this.state.zipCode}/>
+
                 <button onClick={ () => {
-                    this.findCityByZipCode("02169")
+                    this.findCityByZipCode(this.state.zipCode)
+                    // console.log(this.state.zipCode)
+                    console.log(this.state.cityWeather)
+                    this.setState({
+                        searched: true
+                                  })
                 }}>
                     TESTING BUTTON ZIPCODE
                 </button>
                 <br/>
 
-                <button onClick={ () => {
-                    this.findCityByName("Brockton")
-                }}>
-                    TESTING BUTTON CITY NAME
-                </button>
-                <br/>
+                {this.state.searched && this.state.cityWeather && this.state.cityWeather.main &&
+                 <div>
+                    <h1>
+                        City Name: {this.state.cityWeather.name}
+                    </h1>
+                     <h1>
+                         Current Temperature: {this.state.cityWeather.main.temp}
+                     </h1>
+                     <h1>
+                         Temperature feels like: {this.state.cityWeather.main.feels_like}
+                     </h1>
+                     <h1>
+                         Temperature Max: {this.state.cityWeather.main.temp_max}
+                     </h1>
+                     <h1>
+                         Temperature Min: {this.state.cityWeather.main.temp_min}
+                     </h1>
+                     <h1>
+                         Humidity: {this.state.cityWeather.main.humidity}
+                     </h1>
+                 </div>
 
-                <button onClick={ () => {
-                    this.findCityByNameAndState("Chelsea", "MA")
-                }}>
-                    TESTING BUTTON CITY + STATE
-                </button>
-                <br/>
-
-                <button onClick={ () => {
-                    this.findCityByNameAndState("Auburn", "MA")
-                }}>
-                    TESTING BUTTON CITY + STATE + AUBURN
-                </button>
-                <br/>
-
+                }
             </div>
         )
     }
